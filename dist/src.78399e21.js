bf92f2d3952fdf1fabba400417c1ea5127b9b67d
Nicholas <nickis1329@gmail.com>
2024-02-21 13:47:46 -0800

    update

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -29042,7 +29042,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64897" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55936" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
