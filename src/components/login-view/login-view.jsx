d4282f3d4a20e5775545ac56e26e7ac44e3d636a
Nicholas <nickis1329@gmail.com>
2024-03-01 11:38:34 -0800

    Entering data into form fields, binding.
    binding my data to form fields, also created signup-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,15 +1,54 @@
-export const LoginView = () => {
-    return (
-        <form>
-            <label>
-                Username:
-                <input type="text" />
-            </label>
-            <label>
-                Password:
-                <input type="password" />
-            </label>
-            <button type="submit">Submit</button>
-        </form>
+import { useState } from "react";
+
+export const LoginView = ({ onLoggedIn }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    // this prevents the default behavior of the form which is to reload the entire page
+    event.preventDefault();
+
+    const data = {
+      access: username,
+      secret: password
+    };
+
+    fetch("https://myflixdb1329-efa9ef3dfc08.herokuapp.com/login", {
+        method: "POST",
+     headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+            console.log("Login response", data);
+            onLoggedIn(data, user, data.token);
+    })
+    .catch((error) => {
+        console.log("Login failed");
+    });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
     );
-};
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,7 @@
-import { MainView } from "./components/main-view/main-view.jsx";
+import React from 'react';
+import { createRoot } from 'react-dom';
+import MyFlixApp from './MyFlixApp';
 
-// Import statement to indicate that you need to bundle `./index.scss`
-import "./index.scss";
-
-const MyFlixApp = () => {
-  return (
-    <container>
-      <MainView />
-    </container>
-  );
-};
-
-// Find the root of your app
 const container = document.querySelector("#root");
 const root = createRoot(container);
-
 root.render(<MyFlixApp />);
