f475fb15d13d317e52047f914074dcf8f3302d19
Nicholas <nickis1329@gmail.com>
2024-04-18 11:57:50 -0700

    Rows and columns added to MovieView

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,57 +1,34 @@
-import PropTypes from 'prop-types';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
+
+import Button from "react-bootstrap";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 export const MovieView = ({ movie, onBackClick }) => {
-  // Check if movie is null or undefined
-  if (!movie) {
-    return <div>Loading...</div>;
-  }
-
+  if (!movie) return null;
   return (
-    <Row className="my-5 justify-content-center">
-      <Col md={5}>
-        <img src={movie.ImagePath} alt="movie cover" className="img-fluid"/>
-      </Col>
-      <Col md={3}>
-        <div className="my-1">
-          <span className="h1">{movie.Title}</span>
-        </div>
-        <div className="my-1">
-          <span className="h6">Description: </span>
-          <span>{movie.Description}</span>
-        </div>
-        <div className="my-1">
-          <span className="h6">Director: </span>
-          <span>{movie.Director.Name}</span>
-        </div>
-        <div className="my-1">
-          <span className="h6">Genre: </span>
-          <span>{movie.Genre.Name}</span>
-        </div>
-        <div className="my-1">
-          <span className="h6">Year: </span>
-          <span>{movie.Year}</span>
-        </div>
-        <Button onClick={onBackClick} variant="link">Back</Button>
-      </Col>
-    </Row>
-  );
-}
-
-MovieView.propTypes = {
-  movie: PropTypes.shape({
-    ImagePath: PropTypes.string,
-    Title: PropTypes.string,
-    Description: PropTypes.string,
-    Director: PropTypes.shape({
-      Name: PropTypes.string
-    }),
-    Genre: PropTypes.shape({
-      Name: PropTypes.string
-    }),
-    Year: PropTypes.number
-  }),
-  onBackClick: PropTypes.func.isRequired
+ <Row className="movie-view justify-content-md-center">
+    <Col md={8}>
+      <img src={movie.ImagePath} alt="movie poster" class name="img-fluid"/>
+    </Col>
+    <Col md={3}>
+      <div className="m-1">
+        <span className="h6">Title: </span>
+        <span>{movie.Title}</span>
+      </div>
+      <div className="m-1">
+        <span className="h6">Description: </span>
+        <span>{movie.Description}</span>
+      </div>
+      <div className="m-1">
+        <span className="h6">Director: </span>
+        <span>{movie.Director.Name}</span>
+      </div>
+      <div className="m-1-genre">
+        <span className="h6">Genre: </span>
+        <span>{movie.Genre.Name}</span>
+      </div>
+      <Button onClick={onBackClick} variant="link">Back</Button>
+            </Col>
+        </Row>
+    );
 };
