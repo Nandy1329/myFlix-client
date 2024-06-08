9e71ee4cf0cca5cffa9514d03dd696119d9378ea
Nicholas <nickis1329@gmail.com>
2024-06-08 10:40:59 -0700

    added logging

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,83 +1,74 @@
 import React, { useState } from "react";
-import { Form } from "react-bootstrap";
-
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 export const LoginView = ({ onLoggedIn }) => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    
 
-const handleSubmit = (event) => {
-    event.preventDefault();
+    const handleSubmit = (event) => {
+        event.preventDefault();
     
-    
-        fetch('https://myflixdb1329-efa9ef3dfc08.herokuapp.com/login', {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                Username: username,
-                Password: password,
-            }),
-        })
-
+        fetch('https://myflixdb1329-efa9ef3dfc08.herokuapp.com/login?Username=' +
+            username +
+            '&Password=' +
+            password, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ username, password }),
+            })
             .then((response) => response.json())
             .then((data) => {
+                console.log("Login response: ", data);
                 if (data.user) {
                     localStorage.setItem("user", JSON.stringify(data.user));
                     localStorage.setItem("token", data.token);
-                    onLoggedIn(data.user);
+                    onLoggedIn(data.user, data.token);
                 } else {
                     alert("No such user");
                 }
             })
             .catch((error) => {
                 console.error("Error occurred during login", error);
+                if (error.response) {
+                    console.error("Server responded with.", error.response.data);
+                }
                 alert("Something went wrong");
             });
     };
+  return (
+    <Form onSubmit={handleSubmit} className="userLogin">
+      <Form.Group controlId="formUsername" className="inputGroup">
+        <div>
+          <h1 className="userLogin">User Login</h1>
+        </div>
+        <Form.Label>Username:</Form.Label>
+        <Form.Control
+          className="loginForm"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+          autoComplete="off"
+        />
+      </Form.Group>
 
-
-    return (
-        <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="formUsername">
-          <Form.Label>Username:</Form.Label>
-          <Form.Control
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-            minLength="3" 
-          />
-        </Form.Group>
-  
-        <Form.Group controlId="formPassword">
-          <Form.Label>Password:</Form.Label>
-          <Form.Control
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-    );
-  };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      <Form.Group controlId="formPassword" className="inputGroup">
+        <Form.Label>Password:</Form.Label>
+        <Form.Control
+          className="loginForm"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          autoComplete="off"
+        />
+      </Form.Group>
+      <Button variant="primary" type="Submit" className="loginButton">
+        Submit
+      </Button>
+    </Form>
+  );
+};
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -9,140 +9,165 @@ import { Row, Col } from "react-bootstrap";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 export const MainView = () => {
-    const storedUser = JSON.parse(localStorage.getItem("user"));
-    const storedToken = localStorage.getItem("token");
-    const [user, setUser] = useState(storedUser ? storedUser : null);
-    const [token, setToken] = useState(storedToken ? storedToken : null);
-    const [movies, setMovies] = useState([]);
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [user, setUser] = useState(storedUser || null);
+  const [token, setToken] = useState(storedToken || null);
+  const [movies, setMovies] = useState([]);
 
-    useEffect(() => {
-        if (!token) { 
-            return;
+  useEffect(() => {
+    if (!token) {
+      console.log("Token not found");
+      return;
+    }
+
+    console.log("Fetching movies with token:", token);
+
+    fetch("https://myflixdb1329-efa9ef3dfc08.herokuapp.com/movies", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+        'Cache-Control': 'no-cache' // Add this header
+      }
+    })
+      .then((response) => {
+        if (!response.ok) {
+          console.log('Response not OK:', response);
+          throw new Error('Unauthorized');
         }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Movies received from API:", data);
+        if (data.length === 0) {
+          console.log("No movies received.");
+        }
+        const moviesFromApi = data.map((movie) => ({
+          _id: movie._id,
+          Title: movie.Title,
+          ImagePath: movie.ImagePath,
+          Director: {
+            Name: movie.Director.Name,
+            Bio: movie.Director.Bio,
+            Birth: movie.Director.Birth,
+          },
+          Description: movie.Description,
+          ReleaseYear: movie.Year,
+          Genre: movie.Genre.Name,
+        }));
+        setMovies(moviesFromApi);
+        console.log("Movies fetched successfully:", moviesFromApi);
+      })
+      .catch((error) => {
+        console.error("Fetch error:", error);
+        if (error.message === 'Unauthorized') {
+          setUser(null);
+          setToken(null);
+          localStorage.clear();
+        }
+      });
+  }, [token]);
 
-        fetch("https://myflixdb1329-efa9ef3dfc08.herokuapp.com/movies", {
-            headers: { Authorization: `Bearer ${token}` }
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                const moviesFromApi = data.map((movie) => ({
-                    _id: movie._id,
-                    Title: movie.Title,
-                    ImagePath: movie.ImagePath,
-                    Director: {
-                        Name: movie.Director.Name,
-                        Bio: movie.Director.Bio,
-                        Birth: movie.Director.Birth,
-                    },
-                    Description: movie.Description,
-                    ReleaseYear: movie.ReleaseYear,
-                    Genre: movie.Genre.Name,
-                }));
-                setMovies(moviesFromApi);
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    }, [token]);
 
-    return (
-        <BrowserRouter>
-            <NavigationBar
-                user={user}
-                onLoggedOut={() => {
-                    setUser(null);
-                    setToken(null);
-                    localStorage.clear();
-                }}
-            />
+  const handleLoggedIn = (user, token) => {
+    setUser(user);
+    setToken(token);
+    localStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem("token", token);
+  };
 
-            <Row className='justify-content-md-center cards-container'>
-                <Routes>
-                    <Route
-                        path="/signup"
-                        element={
-                            user ? (
-                                <Navigate to="/" />
-                            ) : (
-                                <Col md={5}>
-                                    <SignupView />
-                                </Col>
-                            )
-                        }
-                    />
-                    <Route
-                        path="/login"
-                        element={
-                            user ? (
-                                <Navigate to="/" />
-                            ) : (
-                                <Col md={5}>
-                                    <LoginView onLoggedIn={(user, token) => {
-                                        setUser(user);
-                                        setToken(token);
-                                        localStorage.setItem("user", JSON.stringify(user));
-                                        localStorage.setItem("token", token);
-                                    }} />
-                                </Col>
-                            )
-                        }
-                    />
-                    <Route
-                        path="/movies/:movieId"
-                        element={
-                            !user ? (
-                                <Navigate to="/login" replace />
-                            ) : movies.length === 0 ? (
-                                <Col>The list is empty!</Col>
-                            ) : (
-                                <Col md={8}>
-                                    <MovieView movies={movies} />
-                                </Col>
-                            )
-                        }
-                    />
-                    <Route
-                        path="/"
-                        element={
-                            !user ? (
-                                <Navigate to="/login" replace />
-                            ) : movies.length === 0 ? (
-                                <Col>The list is empty!</Col>
-                            ) : (
-                                <>
-                                    {movies.map((movie) => (
-                                        <Col className="mb-4" key={movie._id} md={3}>
-                                            <MovieCard 
-                                                movie={movie}
-                                                user={user}
-                                                token={token}
-                                                setUser={setUser}
-                                            />
-                                        </Col>
-                                    ))}
-                                </>
-                            )
-                        }
-                    />
-                    <Route
-                        path="/profile"
-                        element={
-                            !user ? (
-                                <Navigate to="/login" replace />
-                            ) : (
-                                <Col md={5}>
-                                    <ProfileView
-                                        movies={movies}
-                                        user={user}
-                                        token={token}
-                                        setUser={setUser}
-                                    />
-                                </Col>
-                            )
-                        }
-                    />
-                </Routes>
-            </Row>
-        </BrowserRouter>
-    );
+  const handleLoggedOut = () => {
+    setUser(null);
+    setToken(null);
+    localStorage.clear();
+  };
+
+  return (
+    <BrowserRouter>
+      <NavigationBar user={user} onLoggedOut={handleLoggedOut} />
+
+      <Row className='justify-content-md-center cards-container'>
+        <Routes>
+          <Route
+            path="/signup"
+            element={
+              user ? (
+                <Navigate to="/" />
+              ) : (
+                <Col md={5}>
+                  <SignupView />
+                </Col>
+              )
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              user ? (
+                <Navigate to="/" />
+              ) : (
+                <Col md={5}>
+                  <LoginView onLoggedIn={handleLoggedIn} />
+                </Col>
+              )
+            }
+          />
+          <Route
+            path="/movies/:movieId"
+            element={
+              !user ? (
+                <Navigate to="/login" replace />
+              ) : movies.length === 0 ? (
+                <Col>The list is empty!</Col>
+              ) : (
+                <Col md={8}>
+                  <MovieView movies={movies} />
+                </Col>
+              )
+            }
+          />
+          <Route
+            path="/"
+            element={
+              !user ? (
+                <Navigate to="/login" replace />
+              ) : movies.length === 0 ? (
+                <Col>The list is empty!</Col>
+              ) : (
+                <>
+                  {movies.map((movie) => (
+                    <Col className="mb-4" key={movie._id} md={3}>
+                      <MovieCard
+                        movie={movie}
+                        user={user}
+                        token={token}
+                        setUser={setUser}
+                      />
+                    </Col>
+                  ))}
+                </>
+              )
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              !user ? (
+                <Navigate to="/login" replace />
+              ) : (
+                <Col md={5}>
+                  <ProfileView
+                    movies={movies}
+                    user={user}
+                    token={token}
+                    setUser={setUser}
+                  />
+                </Col>
+              )
+            }
+          />
+        </Routes>
+      </Row>
+    </BrowserRouter>
+  );
 };
